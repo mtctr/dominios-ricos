39a3787b67251764e1cc8047e32abf7a445c290a
Mateus <mateustargino95@gmail.com>
2020-03-28 12:47:22 -0300

    dominios ricos

---
+++ PaymentContext/PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        //Red, Green, Refactor
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {            
+            var doc = new Document("123",EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {            
+             var doc = new Document("34110468000150",EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {            
+             var doc = new Document("123",EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCPFIsValid()
+        {            
+             var doc = new Document("10454862482",EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+    }
+}
